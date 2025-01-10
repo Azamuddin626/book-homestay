6d72e6d805b109a7a3f7bab656d0fe0e230f4129
Azamuddin626 <abgcik626@gmail.com>
2025-01-10 09:49:12 +0800

    Add comment to layout.blade

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -123,6 +123,7 @@
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{-- Insert content here --}}
                 {{ $slot }}
             </div>
         </main>
