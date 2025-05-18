0658c2e0b64ef086aadc893cf15cf184d06767eb
AthifFitri <athiffitri@gmail.com>
2025-05-18 22:21:39 +0800

    add landing page

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -17,7 +17,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->is_admin) {
+        if (Auth::check() && Auth::user()->roles === 'admin') {
             return $next($request);
         }
 
